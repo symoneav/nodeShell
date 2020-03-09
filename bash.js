cfdff93f4b77251fb0ca59706d15bd30e6fc1cfb
Symone Varnado <varnado@bc.edu>
2020-03-09 14:11:21 -0400

    working file 03_9_20 @1:11pm

--- bash.js
+++ bash.js
@@ -1,1 +1,12 @@
-// initial commit
+process.stdout.write('prompt >')
+process.stdin.on('data',(data)=>{
+    let cmd = data.toString().trim();
+    let invokedFunc = require(`./${cmd}.js`)
+    invokedFunc()
+})
+
+
+
+    
+
+   
